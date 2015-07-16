1976082853420346e057cc2707ae72707df4ff33
Brian Douglas <ilikerobot@gmail.com>
2015-07-15 22:39:58 -0700

    Initial Commit

---
+++ Fakestagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fakestagram
+//
+//  Created by Brian Douglas on 7/15/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
