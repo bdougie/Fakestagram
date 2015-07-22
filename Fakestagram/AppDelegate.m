026d1e46b1b66e8a9d487efb3d3543b569533221
Brian Douglas <ilikerobot@gmail.com>
2015-07-22 16:23:18 -0700

    get photos to show

--- Fakestagram/AppDelegate.m
+++ Fakestagram/AppDelegate.m
@@ -18,7 +18,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ImagesTableViewController alloc] init]];
+    UINavigationController* navigationController = [[UINavigationController alloc] initWithRootViewController:[[ImagesTableViewController alloc] init]];
+    [self.window setRootViewController:navigationController];
+    
+    [self.window makeKeyAndVisible];
+    
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
    
