efee3ab2b8da93265038719c2a448f1718d3b685
Brian Douglas <ilikerobot@gmail.com>
2015-08-23 03:12:10 -0700

    camera button added

---
+++ Fakestagram/CameraViewController.h
@@ -0,0 +1,23 @@
+//
+//  CameraViewController.h
+//  Fakestagram
+//
+//  Created by Brian Douglas on 8/23/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CameraViewController;
+
+@protocol CameraViewControllerDelegate <NSObject>
+
+- (void) cameraViewController:(CameraViewController *)cameraViewController didCompleteWithImage:(UIImage *)image;
+
+@end
+
+@interface CameraViewController : UIViewController
+
+@property (nonatomic, weak) NSObject <CameraViewControllerDelegate> *delegate;
+
+@end
---
+++ Fakestagram/UIImage+ImageUtilities.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+ImageUtilities.h
+//  
+//
+//  Created by Brian Douglas on 8/23/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ImageUtilities)
+
+- (UIImage *) imageWithFixedOrientation;
+- (UIImage *) imageResizedToMatchAspectRatioOfSize:(CGSize)size;
+- (UIImage *) imageCroppedToRect:(CGRect)cropRect;
+
+@end
