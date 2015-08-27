5ed8ffe4de49178c738388886c96e4d3d7a5e5ae
Brian Douglas <ilikerobot@gmail.com>
2015-08-26 17:47:26 -0700

    fixes broken crop lines

--- Fakestagram/CropBox.m
+++ Fakestagram/CropBox.m
@@ -17,10 +17,12 @@
 
 @implementation CropBox
 
-- (id)initWithFrame:(CGRect)frame {
+- (id)initWithFrame:(CGRect)frame
+{
     self = [super initWithFrame:frame];
     if (self) {
-    
+        self.userInteractionEnabled = NO;
+        
         NSArray *lines = [self.horizontalLines arrayByAddingObjectsFromArray:self.verticalLines];
         for (UIView *lineView in lines) {
             [self addSubview:lineView];
@@ -31,13 +33,21 @@
 
 - (NSArray *) horizontalLines {
     if (!_horizontalLines) {
-        _horizontalLines = [self newArrayOfFourthWhiteViews];
+        _horizontalLines = [self newArrayOfFourWhiteViews];
     }
     
     return _horizontalLines;
 }
 
-- (NSArray *) newArrayOfFourthWhiteViews {
+- (NSArray *) verticalLines {
+    if (!_verticalLines) {
+        _verticalLines = [self newArrayOfFourWhiteViews];
+    }
+    
+    return _verticalLines;
+}
+
+- (NSArray *) newArrayOfFourWhiteViews {
     NSMutableArray *array = [NSMutableArray array];
     
     for (int i = 0; i < 4; i++) {
@@ -61,7 +71,7 @@
         
         horizontalLine.frame = CGRectMake(0, (i * thirdOfWidth), width, 0.5);
         
-        CGRect verticalFrame = CGRectMake(i * thirdOfWidth , 0, 0.5, width);
+        CGRect verticalFrame = CGRectMake(i * thirdOfWidth, 0, 0.5, width);
         
         if (i == 3) {
             verticalFrame.origin.x -= 0.5;
