face40dc9ed9e754f13ffd4a62982320651f0ee7
Brian Douglas <ilikerobot@gmail.com>
2015-08-30 08:17:11 -0700

    post-to-instagram

---
+++ Fakestagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Fakestagram
+//
+//  Created by Brian Douglas on 8/28/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
