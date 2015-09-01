6070281b3c63db355c951ce89d5b147803c739f8
Brian Douglas <ilikerobot@gmail.com>
2015-09-01 04:45:55 -0700

    organize files

---
+++ Fakestagram/FilterCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  FilterCollectionView.h
+//  Fakestagram
+//
+//  Created by Brian Douglas on 9/1/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterCollectionView : UICollectionViewCell
+
+@end
---
+++ Fakestagram/FilterCollectionView.m
@@ -0,0 +1,13 @@
+//
+//  FilterCollectionView.m
+//  Fakestagram
+//
+//  Created by Brian Douglas on 9/1/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "FilterCollectionView.h"
+
+@implementation FilterCollectionView
+
+@end
