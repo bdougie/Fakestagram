77f1689a26e3d17410ed09e8f1a2f0da176e4204
Brian Douglas <ilikerobot@gmail.com>
2015-09-06 03:08:00 -0700

    checkpoint 45

--- FakestagramTests/MediaTableViewCellTest.m
+++ FakestagramTests/MediaTableViewCellTest.m
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "MediaTableViewCell.h"
+#import "User.h"
+#import "Media.h"
 
 @interface MediaTableViewCellTest : XCTestCase
 
@@ -25,16 +28,52 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)testThatInitializationWorks {
+    NSDictionary *userDictionary = @{@"id" : @"8675309",
+                                       @"username" : @"d'oh",
+                                       @"full_name" : @"Homer Simpson",
+                                     @"profile_picture" : @"http://www.exampled.com/exanoke.jpg"};
+
+    
+    NSDictionary *mediaDictionary = @{@"id" : @"8675309",
+                                      @"users" : userDictionary,
+                                      @"images" : @{@"standard_resolution" : @{@"url": @"http://exampleinmage.com/1.jpg"}},
+                                      @"caption" : @{@"text" : @"This is text"},
+                                      @"user_has_liked" : @"true",
+                                      @"likes" : @{@"count" : @1                                 }
+                                      };
+    Media *testMedia = [[Media alloc] initWithDictionary:mediaDictionary];
+    
+    XCTAssertEqualObjects(testMedia.idNumber, mediaDictionary[@"id"], @"The id should be equal");
+    XCTAssertEqualObjects(testMedia.user.idNumber, mediaDictionary[@"user"][@"id"], @"The user id should be equal");
+    XCTAssertEqualObjects(testMedia.user.userName, mediaDictionary[@"user"][@"username"], @"the user name should be equal");
+    XCTAssertEqualObjects(testMedia.user.fullName, mediaDictionary[@"user"][@"full_name"], @"the full name should be equal");
+    XCTAssertEqualObjects(testMedia.user.profilePictureURL.absoluteString, mediaDictionary[@"user"][@"profile_picture"], @"the profile picture should be equal");
+    XCTAssertEqualObjects(testMedia.mediaURL.absoluteString, mediaDictionary[@"images"][@"standard_resolution"][@"url"], @"the image url should be equal");
+    XCTAssertEqualObjects(testMedia.caption, mediaDictionary[@"caption"][@"text"], @"the caption text should be equal");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testThatHeightForMediaTableViewCellReturns {
+    NSDictionary *userDictionary = @{@"id" : @"8675309",
+                                       @"username" : @"d'oh",
+                                       @"full_name" : @"Homer Simpson",
+                                     @"profile_picture" : @"http://www.exampled.com/van.jpg"};
+
+    
+    NSDictionary *mediaDictionary = @{@"id" : @"8675309",
+                                      @"users" : userDictionary,
+                                      @"images" : @{@"standard_resolution" : @{@"url": @"http://exampleimage.com/1.jpg"}},
+                                      @"caption" : @{@"text" : @"This is text"},
+                                      @"user_has_liked" : @"true",
+                                      @"likes" : @{@"count" : @1                                 }
+                                      };
+    
+    Media *media = [[Media alloc] initWithDictionary:mediaDictionary];
+    media.image = [UIImage imageNamed:@"1.jpg"];
+    UITraitCollection *traitCollection = [[UIApplication sharedApplication] keyWindow].traitCollection;
+    
+    CGFloat height = [MediaTableViewCell heightForMediaItem:media width:[UIScreen mainScreen].bounds.size.width traitCollection:traitCollection];
+    XCTAssert(height, @"height should be returned");
 }
 
 @end
